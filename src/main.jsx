cfa3aa71799091eafba06eea826474f6a67e506c
Santiago Oronel <saintsantio@gmail.com>
2026-05-28 18:15:16 -0300

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx';
 import { I18nProvider } from './hooks/useI18n.jsx';
 import { ThemeProvider } from './hooks/useTheme.jsx';
 import './styles/index.css';
-import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
